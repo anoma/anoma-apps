f5f45be5b8b5f9b4418bbe0a4ce0fce1b6ca7cf2
Jan Mas Rovira <janmasrovira@gmail.com>
2024-12-23 17:53:12 +0100

    clean documentation

--- Counter/tests/test-init-count-zero.sh
+++ Counter/tests/test-init-count-zero.sh
@@ -6,6 +6,6 @@ set -e
 
 make -C ../ counter-initialize
 sleep 1
-make -C ../ get-count
+make -C ../ counter-get
 diff -w test-init-count-zero.txt ../anoma-build/GetCount.result
 echo "test passed"
--- Counter/tests/test-init-increment-count-one.sh
+++ Counter/tests/test-init-increment-count-one.sh
@@ -8,6 +8,6 @@ make -C ../ counter-initialize
 sleep 2
 make -C ../ counter-increment
 sleep 2
-make -C ../ get-count
+make -C ../ counter-get
 diff -w test-init-increment-count-one.txt ../anoma-build/GetCount.result
 echo "test passed"
