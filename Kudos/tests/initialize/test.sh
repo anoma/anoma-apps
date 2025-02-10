40c32a60d005d451e01cc3d4b9bb6679e286a9ee
Paul Cadman <git@paulcadman.dev>
2025-02-10 14:50:28 +0000

    Wrap stdlib trace so we can switch it off

--- Kudos/tests/initialize/test.sh
+++ Kudos/tests/initialize/test.sh
@@ -9,25 +9,16 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 . "../assert.sh"
 
-# ANOMA_DEBUG=""
+ANOMA_DEBUG=""
 make_dir=../../
 bob=bob
 quantity=12
 
-# make -C $make_dir anoma-start
-# sleep 1
-#
-i=0
-total_quantity=0
-while true
-do
-    printf "****TEST NUMBER: %s *****\n" $i
-    i=$(echo "$i + 1" | bc)
-    total_quantity=$(echo "$total_quantity + $quantity" | bc)
-    latest_block_height=$(make -s -C $make_dir latest-block-height)
-    make -C $make_dir kudos-initialize owner-id=$bob quantity=$quantity
-    predicate=$(printf 'test "true" = $(make -s -C %s block-height=%s has-transaction-after-height)' $make_dir $latest_block_height)
-    poll "$predicate"
-    assert_balance $LINENO $bob "$bob : $total_quantity"
-done
+make -C $make_dir anoma-start
+sleep 1
+latest_block_height=$(make -s -C $make_dir latest-block-height)
+make -C $make_dir kudos-initialize owner-id=$bob quantity=$quantity
+predicate=$(printf 'test "true" = $(make -s -C %s block-height=%s has-transaction-after-height)' $make_dir $latest_block_height)
+poll "$predicate"
+assert_balance $LINENO $bob "$bob : $quantity"
 echo "test passed"
