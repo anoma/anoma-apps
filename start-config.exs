dd9213f10a3d18a894a69f9fd99102b2d12895d1
Jan Mas Rovira <janmasrovira@gmail.com>
2025-03-06 18:43:17 +0100

    remote test

--- start-config.exs
+++ start-config.exs
@@ -1,5 +1,7 @@
-eclient = Anoma.Client.Examples.EClient.create_example_client
-IO.puts("#{eclient.client.grpc_port} #{eclient.node.node_id}")
+# eclient = Anoma.Client.Examples.EClient.create_example_client
+node_id = "babylon control"
+{:ok, eclient} = Anoma.Client.connect("192.248.188.47", 50051, 0, node_id)
+IO.puts("#{eclient.grpc_port} #{node_id}")
 Logger.configure(level: :debug)
-Anoma.Node.Utility.Consensus.start_link(node_id: eclient.node.node_id, interval: 500)
-File.write("config.yaml", "url: localhost\nport: #{eclient.client.grpc_port}\nnodeid: \"\"\n")
+# Anoma.Node.Utility.Consensus.start_link(node_id: eclient.node.node_id, interval: 500)
+File.write("config.yaml", "url: localhost\nport: #{eclient.grpc_port}\nnodeid: \"\"\n")
