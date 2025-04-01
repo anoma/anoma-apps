e68df01f597c1ea677a212a1b7ccfac3852099e4
Paul Cadman <git@paulcadman.dev>
2025-04-01 12:54:45 +0100

    Serialize arguments to prove where required

--- Kudos/web-app/src/index.js
+++ Kudos/web-app/src/index.js
@@ -41,7 +41,38 @@ async function createKudos(ownerId, quantity) {
 
   const kudosCreateProgram = await fetchBinary(kudosCreate);
 
-  const tx = await anomaClient.prove(kudosCreateProgram, [keyPairByteArray, verifyingKeyByteArray, randomBytes, logicProgram, quantity, encodedOwnerIdString]);
+  /**
+   * TODO: The gRPC prove call expects all arguments to be jammed (i.e nock
+   * serialized). However some arguments are already jammed, and these should
+   * not be double jammed.
+   *
+   * You must call `serialize` on all unjammed arguments.
+   *
+   * A better solution would be to use a builder pattern to construct the
+   * arguments to prove:
+   *
+   * builder = new ArgsBuilder();
+   * builder.byteArray(keyPairByteArray);
+   * builder.program(logicProgram);
+   * args = builder.build();
+   * anomaClient.prove(args);
+   *
+   * each method (e.g byteArray) knows whether to jam / serialize the argument.
+   * In this case the `program` method would not serialize, but `byteArray`
+   * would serialize.
+   *
+   * You can see which method to use from the makefile `juvix dev anoma prove` invocation.
+   *
+   * https://github.com/anoma/anoma-apps/blob/fb93dabe868118429d743011050f0544be9d2907/Kudos/makefile#L267-L275
+   *
+   * e.g 'bytearray:' prefix means 'call byteArray method etc.'. Whether each
+   * prefix represents jammed or unjammed data is documented in
+   *
+   * `juvix dev anoma prove --help`.
+   *
+   */
+
+  const tx = await anomaClient.prove(kudosCreateProgram, [serialize(keyPairByteArray), serialize(verifyingKeyByteArray), serialize(zeroRandomBytes), logicProgram, serialize(quantity), serialize(encodedOwnerIdString)]);
   return await anomaClient.addTransaction(tx);
 }
 
