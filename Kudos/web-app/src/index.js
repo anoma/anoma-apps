1d25d752616df73e81f7caa7ea9ca894eaeca992
mauricewbr <39859692+mauricewbr@users.noreply.github.com>
2025-04-01 12:00:28 +0200

    add: create kudos progress sync

--- Kudos/web-app/src/index.js
+++ Kudos/web-app/src/index.js
@@ -1,12 +1,13 @@
-import { AnomaClient, deserializeToString, fetchBinary, serialize, toByteArray, genRandomBytes } from 'anoma-client';
+import { AnomaClient, deserializeToString, fetchBinary, serialize, toByteArray } from 'anoma-client';
 import config from '../config.json';
-import appIdentity from '../nockma/AppIdentity.cued.nockma';
-import getMessage from '../nockma/GetMessage.nockma';
-import helloWorld from '../nockma/HelloWorld.nockma';
-import logic from '../nockma/Logic.proved.nockma';
+// import appIdentity from '../nockma/AppIdentity.cued.nockma';
+// import getMessage from '../nockma/GetMessage.nockma';
+// import helloWorld from '../nockma/HelloWorld.nockma';
 
-import universalKeyPair from '../keys/universalKeyPair';
-import universalVerifyingKey from '../keys/universalVerifyingKey';
+import universalKeyPair from '../keys/universalKeyPair.bin';
+import universalVerifyingKey from '../keys/universalVerifyingKey.bin';
+import kudosCreate from '../nockma/Create.nockma';
+import logic from '../nockma/Logic.proved.nockma';
 
 const grpcServer = `http://${config.proxyHost}:${config.proxyPort}`
 const anomaClient = new AnomaClient(grpcServer);
@@ -20,21 +21,27 @@ async function addMessage(message) {
 
 async function createKudos(ownerId, quantity) {
   // keyPair == signing key + verifying key concatenated
+  console.log(universalKeyPair);
   const uniKeyPairPayload = await fetchBinary(universalKeyPair);
+
   const uniVerifyingKeyPayload = await fetchBinary(universalVerifyingKey);
   const keyPairByteArray = toByteArray(uniKeyPairPayload);
   const verifyingKeyByteArray = toByteArray(uniVerifyingKeyPayload);
 
   const logicProgram = await fetchBinary(logic);
 
-  const randomBytes = genRandomBytes(32);
+  // const randomBytes = genRandomBytes(32);
+  // const randomBytes = new Uint8Array(32);
+  const randomBytes = 0;
+
+  const dummyOwnerId = 'alice';
 
   const encoder = new TextEncoder('utf-8');
   const encodedOwnerIdString = encoder.encode(ownerId);
 
   const kudosCreateProgram = await fetchBinary(kudosCreate);
 
-  const tx = await anomaClient.prove(kudosCreateProgram, [uniSigingKey, uniPublicKey, randomBytes, logicProgram, quantity, encodedOwnerIdString]);
+  const tx = await anomaClient.prove(kudosCreateProgram, [keyPairByteArray, verifyingKeyByteArray, randomBytes, logicProgram, quantity, encodedOwnerIdString]);
   return await anomaClient.addTransaction(tx);
 }
 
@@ -58,6 +65,10 @@ const sendButton = document.getElementById('send-message');
 const responseContainer = document.getElementById('response-container');
 const input = document.getElementById("message-input");
 
+const createKudosButton = document.getElementById('create-kudos');
+const quantityInput = document.getElementById("kudos-quantity-input");
+const ownerIdInput = document.getElementById("kudos-ownerId-input");
+
 button.addEventListener('click', async () => {
   button.disabled = true;
   button.textContent = "Loading..."
@@ -80,20 +91,38 @@ button.addEventListener('click', async () => {
   }
 });
 
-sendButton.addEventListener('click', async () => {
-  sendButton.disabled = true;
-  sendButton.textContent = "Loading..."
-  const message = input.value;
+// sendButton.addEventListener('click', async () => {
+//   sendButton.disabled = true;
+//   sendButton.textContent = "Loading..."
+//   const message = input.value;
+
+//   try {
+//     if (!message.trim()) {
+//       throw Error("Please enter a message")
+//     }
+//     await addMessage(message.trim());
+//   } catch (error) {
+//     responseContainer.innerHTML = `<p style="color: red;">${error}</p>`;
+//   } finally {
+//     sendButton.disabled = false;
+//     sendButton.textContent = "Send Message";
+//   }
+// });
+
+createKudosButton.addEventListener('click', async () => {
+  createKudosButton.disabled = true;
+  createKudosButton.textContent = "Loading..."
+  // const quantity = quantityInput.value;
 
   try {
-    if (!message.trim()) {
-      throw Error("Please enter a message")
-    }
-    await addMessage(message.trim());
+    // if (!quantity.trim()) {
+    //   throw Error("Please enter a message")
+    // }
+    await createKudos("alice", 2);
   } catch (error) {
     responseContainer.innerHTML = `<p style="color: red;">${error}</p>`;
   } finally {
-    sendButton.disabled = false;
-    sendButton.textContent = "Send Message";
+    createKudosButton.disabled = false;
+    createKudosButton.textContent = "Create Kudos";
   }
 });
