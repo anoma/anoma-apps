fc57bb94a1df75c21fef860ad67e658697498498
Paul Cadman <git@paulcadman.dev>
2025-04-01 12:58:15 +0100

    Remove unused code from kudos webapp index.js

--- Kudos/web-app/src/index.js
+++ Kudos/web-app/src/index.js
@@ -1,8 +1,5 @@
 import { AnomaClient, deserializeToString, fetchBinary, serialize, toByteArray } from 'anoma-client';
 import config from '../config.json';
-// import appIdentity from '../nockma/AppIdentity.cued.nockma';
-// import getMessage from '../nockma/GetMessage.nockma';
-// import helloWorld from '../nockma/HelloWorld.nockma';
 
 import universalKeyPair from '../keys/universalKeyPair.bin';
 import universalVerifyingKey from '../keys/universalVerifyingKey.bin';
@@ -11,13 +8,6 @@ import logic from '../nockma/Logic.proved.nockma';
 
 const grpcServer = `http://${config.proxyHost}:${config.proxyPort}`
 const anomaClient = new AnomaClient(grpcServer);
-
-async function addMessage(message) {
-  const logicProgram = await fetchBinary(logic);
-  const helloWorldProgram = await fetchBinary(helloWorld);
-  const tx = await anomaClient.prove(helloWorldProgram, [logicProgram, serialize(message)]);
-  return await anomaClient.addTransaction(tx);
-}
 
 async function createKudos(ownerId, quantity) {
   // keyPair == signing key + verifying key concatenated
@@ -99,46 +89,6 @@ const input = document.getElementById("message-input");
 const createKudosButton = document.getElementById('create-kudos');
 const quantityInput = document.getElementById("kudos-quantity-input");
 const ownerIdInput = document.getElementById("kudos-ownerId-input");
-
-button.addEventListener('click', async () => {
-  button.disabled = true;
-  button.textContent = "Loading..."
-  try {
-    let messages = await getMessages();
-    const list = document.createElement('ul');
-    messages.sort();
-    for (const m of messages) {
-      const listItem = document.createElement('li');
-      listItem.textContent = m;
-      list.appendChild(listItem);
-    }
-    responseContainer.innerHTML = '';
-    responseContainer.appendChild(list);
-  } catch (error) {
-    responseContainer.innerHTML = `<p style="color: red;">${error}</p>`;
-  } finally {
-    button.disabled = false;
-    button.textContent = "Get Messages";
-  }
-});
-
-// sendButton.addEventListener('click', async () => {
-//   sendButton.disabled = true;
-//   sendButton.textContent = "Loading..."
-//   const message = input.value;
-
-//   try {
-//     if (!message.trim()) {
-//       throw Error("Please enter a message")
-//     }
-//     await addMessage(message.trim());
-//   } catch (error) {
-//     responseContainer.innerHTML = `<p style="color: red;">${error}</p>`;
-//   } finally {
-//     sendButton.disabled = false;
-//     sendButton.textContent = "Send Message";
-//   }
-// });
 
 createKudosButton.addEventListener('click', async () => {
   createKudosButton.disabled = true;
