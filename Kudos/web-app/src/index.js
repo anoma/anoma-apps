25ed4d4161b979dc894446ce06d9f895acad403b
mauricewbr <39859692+mauricewbr@users.noreply.github.com>
2025-04-02 18:07:06 +0200

    add: using local storage and key gen

--- Kudos/web-app/src/index.js
+++ Kudos/web-app/src/index.js
@@ -1,20 +1,37 @@
 import { AnomaClient, deserializeToString, fetchBinary, genRandomBytes, ProveArgsBuilder } from 'anoma-client';
 import config from '../config.json';
 
-import universalKeyPair from '../keys/universalKeyPair.bin';
-import universalVerifyingKey from '../keys/universalVerifyingKey.bin';
 import kudosCreate from '../nockma/Create.nockma';
 import logic from '../nockma/Logic.proved.nockma';
 
 import getBalance from '../nockma/GetBalance.nockma';
 
+import { Keypair } from "./keys";
+import { Store } from "./storage";
+
+const LOCALSTORAGE_PREFIX = "kudos-localstorage";
+const KEYSTORE_KEY = "kudos-keystore";
+const store = new Store(KEYSTORE_KEY, LOCALSTORAGE_PREFIX);
+
 const grpcServer = `http://${config.proxyHost}:${config.proxyPort}`
 const anomaClient = new AnomaClient(grpcServer);
 
 async function createKudos(ownerId, quantity) {
+  const keyRecord = await store.get(ownerId)
+  let keyInstance;
+  if (!keyRecord) {
+    keyInstance = await Keypair.genEd25519Keypair(ownerId);
+    await store.add(ownerId, keyInstance.values);
+  } else {
+    keyInstance = Keypair.fromStoredKeypair(keyRecord);
+  }
+
+  const privateKey = keyInstance.privateKey.bytes
+  const publicKey = keyInstance.publicKey.bytes
+
   const proveArgs = new ProveArgsBuilder()
-    .bytearray(await fetchBinary(universalKeyPair))
-    .bytearray(await fetchBinary(universalVerifyingKey))
+    .bytearray(new Uint8Array([...privateKey, ...publicKey]))
+    .bytearray(publicKey)
     .bytesUnjammed(genRandomBytes(32))
     .bytes(await fetchBinary(logic))
     .nat(quantity)
@@ -25,12 +42,17 @@ async function createKudos(ownerId, quantity) {
 }
 
 async function getBalances(ownerId) {
-  const pubKeyUrl = universalVerifyingKey;
-  // Filter arguments are NOT jammed
-  const pubKey = await fetchBinary(pubKeyUrl)
+  const keyRecord = await store.get(ownerId)
+  let keyInstance;
+  if (!keyRecord) {
+    throw Error("There are no keys associated with ownerId")
+  } else {
+    keyInstance = Keypair.fromStoredKeypair(keyRecord);
+  }
+
   // TODO: Additionally filter by Kudos Kind, i.e. replace [] by [kind]
-  const rawBalances = await anomaClient.filter([], [pubKey]);
-  if (rawBalances.legth == 0) {
+  const rawBalances = await anomaClient.filter([], [keyInstance.publicKey.bytes]);
+  if (rawBalances.length == 0) {
     throw Error("There are no stored balances");
   }
   const getBalancesProgram = await fetchBinary(getBalance);
@@ -42,6 +64,9 @@ async function getBalances(ownerId) {
   return txResult.data;
 }
 
+// const keyAliasInput = document.getElementById("key-alias");
+// const keygenButton = document.getElementById("gen-keypair");
+
 const responseContainer = document.getElementById('response-container');
 const createKudosButton = document.getElementById('create-kudos');
 const getBalanceButton = document.getElementById("get-balance");
@@ -49,28 +74,28 @@ const ownerIdInput = document.getElementById("owner-id-input");
 const quantityInput = document.getElementById("quantity-input");
 const balanceOwnerInput = document.getElementById("balance-owner-input");
 
-createKudosButton.addEventListener('click', async () => {
-  createKudosButton.disabled = true;
-  createKudosButton.textContent = "Loading..."
-  const ownerId = ownerIdInput.value;
-  const quantity = quantityInput.value;
+// keygenButton.addEventListener("click", async () => {
+//   const alias = keyAliasInput.value.trim();
+//   if (alias === "") {
+//     setError("Alias was not provided!");
+//     return;
+//   }
 
-  try {
-    if (!ownerId.trim()) {
-      throw Error("Please enter an owner id")
-    }
-    if (!quantity.trim()) {
-      throw Error("Please enter a quantity")
-    }
-    await createKudos(ownerId, parseInt(quantity));
-  }
-  catch (error) {
-    responseContainer.innerHTML = `<p style="color: red;">${error}</p>`;
-  } finally {
-    createKudosButton.disabled = false;
-    createKudosButton.textContent = "Create Kudos";
-  }
-});
+//   if (await store.get(alias)) {
+//     setError(`Keypair for ${alias} exists! Choose a different name.`);
+//     return;
+//   }
+
+//   try {
+//     setError("");
+
+//     const keypair = await Keypair.genEd25519Keypair(alias);
+//     await store.add(alias, keypair.values);
+//     updateList(await getRecords(store));
+//   } catch (e) {
+//     setError(console.error(e));
+//   }
+// });
 
 getBalanceButton.addEventListener('click', async () => {
   getBalanceButton.disabled = true;
@@ -94,5 +119,28 @@ getBalanceButton.addEventListener('click', async () => {
   } finally {
     getBalanceButton.disabled = false;
     getBalanceButton.textContent = "Get Balance";
+  }
+});
+
+createKudosButton.addEventListener('click', async () => {
+  createKudosButton.disabled = true;
+  createKudosButton.textContent = "Loading..."
+  const ownerId = ownerIdInput.value;
+  const quantity = quantityInput.value;
+
+  try {
+    if (!ownerId.trim()) {
+      throw Error("Please enter an owner id")
+    }
+    if (!quantity.trim()) {
+      throw Error("Please enter a quantity")
+    }
+    await createKudos(ownerId, parseInt(quantity));
+  }
+  catch (error) {
+    responseContainer.innerHTML = `<p style="color: red;">${error}</p>`;
+  } finally {
+    createKudosButton.disabled = false;
+    createKudosButton.textContent = "Create Kudos";
   }
 });
