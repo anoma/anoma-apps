9a2095d56afe56209677a913050fe72a19a68013
Lukasz Czajka <lukasz@heliax.dev>
2025-05-09 14:01:54 +0200

    include shielded tests in the test suite

--- run-tests.sh
+++ run-tests.sh
@@ -3,5 +3,5 @@
 parallel --line-buffer --tag --joblog parallel-timings.log \
   'TMPDIR=$(mktemp -d); trap "XDG_CONFIG_HOME=$TMPDIR juvix dev anoma stop || true; rm -rf $TMPDIR" EXIT; \
    cd {1} && XDG_CONFIG_HOME=$TMPDIR ./all-tests.sh 2>&1 | tee all-tests.log' \
-  ::: HelloWorld/tests SimpleHelloWorld/tests Counters/Counter/Simple/tests Counters/Counter/Unique/tests Kudos/tests
+  ::: HelloWorld/tests SimpleHelloWorld/tests ShieldedHelloWorld/tests Counters/Counter/Simple/tests Counters/Counter/Unique/tests Kudos/tests
 
